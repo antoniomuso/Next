a47c407e8bae85c38388e424c583f6d5a0ec6b3f
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-07-04 16:44:07 +0200

    Added snap gesture

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.Gestures.Endpoint;
+using Microsoft.Gestures;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.Gestures.Stock.Gestures;
+
+namespace ConsoleManaged
+{
+    class Program
+    {
+        private static GesturesServiceEndpoint _gesturesService;
+        private static Gesture _snapGesture;
+
+        static void Main(string[] args)
+        {       
+            Console.Title = "GesturesServiceStatus[Initializing]";
+            Console.WriteLine("Execute one of the following gestures: Like, Drop-the-Mic, Rotate-Right ! press 'ctrl+c' to exit");
+
+            // One can optionally pass the hostname/IP address where the gestures service is hosted
+            var gesturesServiceHostName = !args.Any() ? "localhost" : args[0];
+            RegisterGestures(gesturesServiceHostName).Wait();
+            Console.ReadKey();
+        }
+
+        private static async Task RegisterGestures(string gesturesServiceHostName)
+        {
+            // Step 1: Connect to Microsoft Gestures service            
+            _gesturesService = GesturesServiceEndpointFactory.Create(gesturesServiceHostName);            
+            _gesturesService.StatusChanged += (s, arg) => Console.Title = $"GesturesServiceStatus [{arg.Status}]";            
+            await _gesturesService.ConnectAsync();
+
+            // Step 2: Define bunch of custom Gestures, each detection of the gesture will emit some message into the console
+            await RegisterSnapGesture();
+        }
+
+         private static async Task RegisterSnapGesture() 
+        {
+            // Start with defining the first pose, ...
+            // ... define the second pose, ...
+            // ... finally define the gesture using the hand pose objects defined above forming a simple state machine
+            _snapGesture = new FingerSnapGesture();
+            _snapGesture.Triggered += (s, e) => OnGestureDetected(s, e, ConsoleColor.Yellow);
+
+            // Step 3: Register the gesture             
+            // Registering the like gesture _globally_ (i.e. isGlobal:true), by global registration we mean this gesture will be 
+            // detected even it was initiated not by this application or if the this application isn't in focus
+            await _gesturesService.RegisterGesture(_snapGesture, isGlobal: true);
+        }
+
+        private static void OnGestureDetected(object sender, GestureSegmentTriggeredEventArgs args, ConsoleColor foregroundColor)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Gesture detected! : ");
+            Console.ForegroundColor = foregroundColor;
+            Console.WriteLine(args.GestureSegment.Name);
+            Console.ResetColor();
+        }
+    }
+}
