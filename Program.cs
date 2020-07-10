f7cbcf0cea05267f26a9fba5779c6f39e4ade64b
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-07-10 16:05:48 +0200

    New Gestures

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using Microsoft.Gestures;
 using System.Threading.Tasks;
 using System.Linq;
 using Microsoft.Gestures.Stock.Gestures;
-using Microsoft.Gestures.Stock.HandPoses;
 using Microsoft.CognitiveServices.Speech;
 using System.Text.RegularExpressions;
 
@@ -14,9 +13,8 @@ namespace ConsoleManaged
     {
         private static GesturesServiceEndpoint _gesturesService;
         private static Gesture _snapGesture;
-        private static Gesture _swipeGesture;
-        private static Gesture _clockwiseCircleGesture;
-        private static Gesture _counterClockwiseCircleGesture;
+        private static Gesture _rotateRightGesture;
+        private static Gesture _rotateLeftGesture;
         private static Keyboard keyboard;
         private static SpeechRecognizer recognizer;
 
@@ -47,73 +45,61 @@ namespace ConsoleManaged
 
             // Step 2: Define bunch of custom Gestures, each detection of the gesture will emit some message into the console
             await RegisterFingerSnapGesture();
-            await RegisterSwipeGesture("SwipeLeftGesture", PoseDirection.Left);
-            //await RegisterClockwiseCircleGesture();
-            //await RegisterCounterClockwiseCircleGesture();
+            await RegisterRotateRightGesture();
+            await RegisterRotateLeftGesture();
         }
 
         private static async Task RegisterFingerSnapGesture()
         {
             // Step 3: Register the gesture
             _snapGesture = new FingerSnapGesture();
-            _snapGesture.Triggered += (s, e) => OnGestureDetected(s, e, ConsoleColor.Yellow);
+            _snapGesture.Triggered += (s, e) => OnGestureDetected(s, e, ConsoleColor.Green);
 
             // Registering the like gesture _globally_ (i.e. isGlobal:true), by global registration we mean this gesture will be 
             // detected even it was initiated not by this application or if the this application isn't in focus
             await _gesturesService.RegisterGesture(_snapGesture, isGlobal: true);
         }
 
-        private static async Task RegisterSwipeGesture(string name, PoseDirection direction)
+        private static async Task RegisterRotateRightGesture()
         {
-            // Start with defining the first pose, ..
-            var fingerSet = new HandPose("FingersSet", new PalmPose(new AnyHandContext(), direction, orientation: PoseDirection.Forward),
-                                                       new FingertipDistanceRelation(Finger.Middle, RelativeDistance.Touching, new[] { Finger.Index, Finger.Ring }),
-                                                       new FingerPose(new[] { Finger.Index, Finger.Middle, Finger.Ring }, PoseDirection.Forward));
-
+            // Start with defining the first pose, ...
+            var hold = new HandPose("Hold", new FingerPose(new[] { Finger.Thumb, Finger.Index }, FingerFlexion.Open, PoseDirection.Forward),
+                                            new FingertipDistanceRelation(Finger.Index, RelativeDistance.NotTouching, Finger.Thumb),
+                                            new FingertipPlacementRelation(Finger.Index, RelativePlacement.Right, Finger.Thumb));
             // ... define the second pose, ...
-            var fingersBent = new HandPose("FingersBent", new PalmPose(new AnyHandContext(), direction, orientation: PoseDirection.Forward),
-                                                          new FingertipDistanceRelation(Finger.Middle, RelativeDistance.Touching, new[] { Finger.Index, Finger.Ring }),
-                                                          new FingerPose(new[] { Finger.Index, Finger.Middle, Finger.Ring }, direction | PoseDirection.Backward));
+            var rotate = new HandPose("Rotate", new FingerPose(new[] { Finger.Thumb, Finger.Index }, FingerFlexion.Open, PoseDirection.Forward),
+                                                new FingertipDistanceRelation(Finger.Index, RelativeDistance.NotTouching, Finger.Thumb),
+                                                new FingertipPlacementRelation(Finger.Index, RelativePlacement.Below, Finger.Thumb));
 
             // ... finally define the gesture using the hand pose objects defined above forming a simple state machine
-            _swipeGesture = new Gesture(name, fingerSet, fingersBent);
-            _swipeGesture.Triggered += (s, e) => OnGestureDetected(s, e, ConsoleColor.Blue);
+            _rotateRightGesture = new Gesture("RotateRight", hold, rotate);
+            _rotateRightGesture.Triggered += (s, e) => OnGestureDetected(s, e, ConsoleColor.Blue);
 
+            // Step 3: Register the gesture             
             // Registering the like gesture _globally_ (i.e. isGlobal:true), by global registration we mean this gesture will be 
             // detected even it was initiated not by this application or if the this application isn't in focus
-            await _gesturesService.RegisterGesture(_swipeGesture, isGlobal: true);
+            await _gesturesService.RegisterGesture(_rotateRightGesture, isGlobal: true);
         }
 
-        private static async Task RegisterClockwiseCircleGesture()
+        private static async Task RegisterRotateLeftGesture()
         {
-            // Step 3: Register the gesture
-            _clockwiseCircleGesture = new Gesture("ClockwiseCircleGesture",
-                                                  new PointingFingerPose("Point"),
-                                                  new HandMotion("ClockwiseCircle", new[] { VerticalMotionSegment.ClockwiseArcRightUpward,
-                                                                                            VerticalMotionSegment.ClockwiseArcRightDownward,
-                                                                                            VerticalMotionSegment.ClockwiseArcLeftDownward,
-                                                                                            VerticalMotionSegment.ClockwiseArcLeftUpward }));
-            _clockwiseCircleGesture.Triggered += (s, e) => OnGestureDetected(s, e, ConsoleColor.Blue);
+            // Start with defining the first pose, ...
+            var hold = new HandPose("Hold", new FingerPose(new[] { Finger.Thumb, Finger.Index }, FingerFlexion.Open, PoseDirection.Forward),
+                                            new FingertipDistanceRelation(Finger.Index, RelativeDistance.NotTouching, Finger.Thumb),
+                                            new FingertipPlacementRelation(Finger.Index, RelativePlacement.Right, Finger.Thumb));
+            // ... define the second pose, ...
+            var rotate = new HandPose("Rotate", new FingerPose(new[] { Finger.Thumb, Finger.Index }, FingerFlexion.Open, PoseDirection.Forward),
+                                                new FingertipDistanceRelation(Finger.Index, RelativeDistance.NotTouching, Finger.Thumb),
+                                                new FingertipPlacementRelation(Finger.Index, RelativePlacement.Above, Finger.Thumb));
 
+            // ... finally define the gesture using the hand pose objects defined above forming a simple state machine
+            _rotateLeftGesture = new Gesture("RotateLeft", hold, rotate);
+            _rotateLeftGesture.Triggered += (s, e) => OnGestureDetected(s, e, ConsoleColor.Yellow);
+
+            // Step 3: Register the gesture             
             // Registering the like gesture _globally_ (i.e. isGlobal:true), by global registration we mean this gesture will be 
             // detected even it was initiated not by this application or if the this application isn't in focus
-            await _gesturesService.RegisterGesture(_clockwiseCircleGesture, isGlobal: true);
-        }
-
-        private static async Task RegisterCounterClockwiseCircleGesture()
-        {
-            // Step 3: Register the gesture
-            _counterClockwiseCircleGesture = new Gesture("CounterClockwiseCircleGesture",
-                                                         new PointingFingerPose("Point"),
-                                                         new HandMotion("CounterClockwiseCircle", new[] { VerticalMotionSegment.CounterClockwiseArcRightDownward,
-                                                                                                          VerticalMotionSegment.CounterClockwiseArcRightUpward,
-                                                                                                          VerticalMotionSegment.CounterClockwiseArcLeftUpward,
-                                                                                                          VerticalMotionSegment.CounterClockwiseArcLeftDownward }));
-            _counterClockwiseCircleGesture.Triggered += (s, e) => OnGestureDetected(s, e, ConsoleColor.Green);
-
-            // Registering the like gesture _globally_ (i.e. isGlobal:true), by global registration we mean this gesture will be 
-            // detected even it was initiated not by this application or if the this application isn't in focus
-            await _gesturesService.RegisterGesture( _counterClockwiseCircleGesture, isGlobal: true);
+            await _gesturesService.RegisterGesture(_rotateLeftGesture, isGlobal: true);
         }
 
         private static void OnGestureDetected(object sender, GestureSegmentTriggeredEventArgs args, ConsoleColor foregroundColor)
@@ -126,7 +112,9 @@ namespace ConsoleManaged
 
             if (args.GestureSegment.Name == "FingerSnapGesture")
                 keyboard.Send(Keyboard.VirtualKeyShort.RIGHT);
-            else if (args.GestureSegment.Name == "SwipeLeftGesture")
+            else if (args.GestureSegment.Name == "RotateRight")
+                keyboard.Send(Keyboard.VirtualKeyShort.RIGHT);
+            else if (args.GestureSegment.Name == "RotateLeft")
                 keyboard.Send(Keyboard.VirtualKeyShort.LEFT);
         }
 
